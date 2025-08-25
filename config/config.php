fd0840f9345b526c947b28dafc1be2873192fb10
Marmiril <141357341+Marmiril@users.noreply.github.com>
2025-08-25 17:51:05 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+// Base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'taleteller_db');
+
+
+define('ENVIRONMENT', 'development');
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
